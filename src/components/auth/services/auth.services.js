20ed0c0102258fd064020cad9a7cc815226b9677
Emiliano <emilianogorgellon49@gmail.com>
2022-05-04 11:20:11 -0300

    Modify nodemailer

--- src/components/auth/services/auth.services.js
+++ src/components/auth/services/auth.services.js
@@ -6,10 +6,9 @@ const Nodemailer = require("../../../utils/nodemailer/nodemailer");
 class Auth {
     async signUpService(data) {
         try {
-            // const userFound = await this.getUserByEmail(data.body.email);
-            // if (userFound.length !== 0) return new Error("Error ya existe un usuario con ese email");
-            const a = await this.saveUser(data);
-            console.log(a);
+            const userFound = await this.getUserByEmail(data.body.email);
+            if (userFound.length !== 0) return new Error("Error ya existe un usuario con ese email");
+            await this.saveUser(data);
             return "xd";
         } catch (error) {
             console.log(error);
@@ -33,20 +32,19 @@ class Auth {
         try {
             const {name, email, password, isAdmin } = data.body;
             const newPassword = await bcrypter.encryptPassword(password);
-            // const result_cloudinary = Cloudinary.generateImagen(data.image.path);
+            const result_cloudinary = Cloudinary.generateImagen(data.image.path);
             const newUser = {
                 name,
                 email,
                 password: newPassword,
-                // imageUrl: result_cloudinary[0],
-                // public_id: result_cloudinary[1],
+                imageUrl: result_cloudinary[0],
+                public_id: result_cloudinary[1],
                 isAdmin: isAdmin ? true : false,
                 created_at: new Date()
             }
-            console.log("Ya cree la imagen")
-            // const result = await usersModel.create(newUser);
+            const result = await usersModel.create(newUser);
             await Nodemailer.new_user_verification(newUser);
-            // return await JWT.generateToken({name:result.name, email: result.email, image: result. imageUrl, isAdmin: result.isAdmin})
+            return await JWT.generateToken({name:result.name, email: result.email, image: result. imageUrl, isAdmin: result.isAdmin})
         } catch (error) {
             console.log("Error en crear usuario");
             return new Error("Error in system")
--- src/utils/nodemailer/nodemailer.js
+++ src/utils/nodemailer/nodemailer.js
@@ -1,8 +1,8 @@
-const nodemailer = require("nodemailer");
+const {createTransport} = require("nodemailer");
 const { config } = require("../../config");
 const user = config.nodemailer_user;
 const pass = config.nodemailer_pw;
-nodemailer.createTransport({
+const transport = createTransport({
     service: 'gmail',
     port: 587,
     secure: true,
@@ -15,28 +15,27 @@ nodemailer.createTransport({
 class Nodemailer {
     async new_user_verification (data){
         try {
-            console.log(" a putno de denviar email")
             const subject = "Verificacion de Email";
             const html = 
                 `
-                    <form method="post" action="http://localhost:3000/api/carrito/" style="display: block; width: 100%;">
+                    <form method="post" action="http://localhost:3000" style="display: block; width: 100%;">
                         <h1 style="text-align: center;">Hola ${data.name}, haga click en el siguiente enlace para confirmar su cuenta</h1>
-                        <div style="display: flex; justify-content: space-evenly; width: 80%;">
-                            <img style="width: 75px; object-fit: cover; border-radius: 50%;" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/wine-bar-logo-brand-logo-template-design-467de904bec2855b56d72d5f58c91407_screen.jpg?ts=1574430397" alt="logo vino" />
-                            <img style="width: 75px; object-fit: cover; border-radius: 50%;" src="${data.imageUrl}" alt="img-avatar" />
+                        <div style="display: flex; justify-content: center; width: 100%;">
+                            <img style="width: 75px; object-fit: cover; margin: 5%;" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/wine-bar-logo-brand-logo-template-design-467de904bec2855b56d72d5f58c91407_screen.jpg?ts=1574430397" alt="logo vino" />
+                            <img style="width: 75px; object-fit: cover; margin: 5%;" src="https://res.cloudinary.com/dtt60hh34/image/upload/v1651524775/k4yhpn7axpbsl3dqpogx.png    " alt="img-avatar" />
                         </div>
-                        <button type="submit" style="padding: 10px 20px; text-align: center; border-radius: 6px; font-size: 24px; background-color: #0077F9;">Confirmar Email</button>
+                        <button type="submit" style="padding: 5px 10px; text-align: center; border-radius: 6px; font-size: 18px; background-color: #0077F9;">Confirmar Email</button>
                     </form>
                 `; 
-            const to = user;
+            
             return await transport.sendMail({
-                from:"E-commerce de Emilano <emilianogorgellon@gmail.com>",
-                to,
+                from:`E-commerce de Emilano <${user}>`,
+                to: `${data.email}`,
                 subject,
                 html
             });
         } catch (error) {
-            throw new Error("Error en enviar email");
+            throw new Error(`Error en enviar email: ${error}`);
         }
     }
 }

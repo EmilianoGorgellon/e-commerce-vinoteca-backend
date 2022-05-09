21d5836e0c23681b4c5309643f2b5825312881c4
Emiliano <emilianogorgellon49@gmail.com>
2022-05-09 10:12:33 -0300

    Modify cloudinary

--- src/utils/cloudinary/cloudinary.js
+++ src/utils/cloudinary/cloudinary.js
@@ -1,12 +1,13 @@
 const cloudinary_config = require("../../config/cloudinary");
-
+const fs = require("fs-extra");
 class Cloudinary {
     async generateImagen (image) {
         try {
             const result_cloudinary = await cloudinary_config.v2.uploader.upload(image);
+            await fs.unlink(image);
             const imageUrl = result_cloudinary.secure_url;
             const public_id = result_cloudinary.public_id;
-            return [imageUrl, public_id]
+            return [imageUrl, public_id];
         } catch (error) {
             throw new Error("No se creo imagen: ", error)
         }
